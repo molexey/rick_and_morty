89952af212329871c28510e72a0e7962f110cf35
Alexey Moskalenko <a.moskalenko@appricot.ru>
2023-02-14 17:45:55 +0300

    Added character details screen

--- lib/api/characteres/characteres_api.dart
+++ lib/api/characteres/characteres_api.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:rick_and_morty/api/characteres/data/characters_response_dto.dart';
+import 'package:rick_and_morty/features/characteres/entity/character_entity.dart';
 
 class CharactersApi {
   final dio = Dio();
@@ -12,6 +13,21 @@ class CharactersApi {
           .get('https://rickandmortyapi.com/api/character/?page=$page');
 
       final data = CharactersResponseDto.fromJson(response.data);
+      return data;
+    } on DioError catch (error) {
+      print(error.message);
+      return null;
+    }
+  }
+
+  Future<CharacterEntity?> getCharacter({
+  required int id,
+}) async {
+    try {
+      final response = await dio
+          .get('https://rickandmortyapi.com/api/character/$id');
+
+      final data = CharacterEntity.fromJson(response.data);
       return data;
     } on DioError catch (error) {
       print(error.message);
--- lib/features/character_details/presentation/character_details_screen.dart
+++ lib/features/character_details/presentation/character_details_screen.dart
@@ -1,14 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty/features/character_details/repository/character_repository.dart';
+import 'package:rick_and_morty/features/characteres/entity/character_entity.dart';
 
-class CharacterDetailsScreen extends StatelessWidget {
+class CharacterDetailsScreen extends StatefulWidget {
   final int id;
   const CharacterDetailsScreen({Key? key, required this.id}) : super(key: key);
 
   @override
+  State<CharacterDetailsScreen> createState() => _CharacterDetailsScreenState();
+}
+
+class _CharacterDetailsScreenState extends State<CharacterDetailsScreen> {
+  final _characterRepository = CharacterResository();
+
+  Future<CharacterEntity?> _getCharacter(int id) async {
+    print('!!! 1 _getCharacter()');
+    return await _characterRepository.getCharacter(id);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text('Character ${id.toString()}'),
-    );
+        appBar: AppBar(),
+        body: FutureBuilder<CharacterEntity?>(
+            future: _getCharacter(widget.id),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data != null) {
+                final item = snapshot.data!;
+                return Center(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).padding.bottom),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(16)),
+                            child: Image.network(
+                              item.image,
+                              width: 300,
+                              height: 300,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          Text(item.name, style: const TextStyle(fontSize: 20)),
+                          Text('${item.status} — ${item.species}'),
+                          Text('Last known location: ${item.location.name}'),
+                          Text('Gender: ${item.gender}'),
+                          Text('Number of episodes: ${item.episode.length}')
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }
+              return const Center(child: CupertinoActivityIndicator());
+            }));
   }
 }
